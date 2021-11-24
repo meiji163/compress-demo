783ee3608447feef8c1f86c55f1b5d0f2ff115b5
meiji163 <mysatellite99@gmail.com>
2021-11-24 08:29:46 -0800

    add comments

--- compress.py
+++ compress.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-from typing import List, Dict
+#!/usr/bin/env python3
+from typing import List
 from argparse import ArgumentParser
-parser = ArgumentParser(description = "dict substitution encoding")
+parser = ArgumentParser(description = "prefix code compression")
 parser.add_argument("-x", dest="expand", action="store_true")
 parser.add_argument("file", type=str)
 args = parser.parse_args()
 
 # a binary prefix code i made up
+# ( i.e. no codeword is a prefix of any other )
 ENDL = "!\n"
 CODE = {
          "MLH": "10",
@@ -34,6 +35,8 @@ def encode(sents: List[List[str]]) -> bytes:
                 codes.append(CODE[t])
         codes.append(CODE[ENDL])
     out = "".join(codes)
+
+    # convert the binary string to actual bytes  
     enc_bytes = int(out, 2).to_bytes( (len(out)+7)//8, byteorder='big')
     return enc_bytes 
 
@@ -42,12 +45,16 @@ def decode(data: bytes) -> List[str]:
     code = code.lstrip("0b")
     words = {code: w for w, code in CODE.items() }
     decoded = []
+
+    # read the prefix, it should be uniquely decoable!
     while len(code) > 0:
         valid = False 
         for prefix in words:
             if code.startswith(prefix):
                 decoded.append(words[prefix])
                 code = code[len(prefix):]
+
+                # these words always followed by "is"
                 if words[prefix] in ("MLH", "Pod 4.2.0", "compression"):
                     decoded.append("is")
                 valid = True
@@ -57,6 +64,7 @@ def decode(data: bytes) -> List[str]:
     return decoded
 
 if __name__ == "__main__":
+    # decompress the file
     if args.expand:
         with open(args.file, 'rb') as f:
             data = f.read()
@@ -65,8 +73,12 @@ if __name__ == "__main__":
         with open(out_file,'w') as f:
             for i in range(len(decoded)):
                 f.write(decoded[i])
+                
+                # fill in the spaces
                 if decoded[i]!=ENDL and i+1<len(decoded) and decoded[i+1]!=ENDL:
                     f.write(" ")
+
+    # compress the file 
     else:
         with open(args.file, 'r') as f:
             lines = f.readlines()
